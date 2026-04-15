757c84127d34dd435641338a6870f0aefb482e2c
Rémi Campistron <remi.campistron@gmail.com>
2026-04-15 15:10:14 +0200

    Ajuste le formatage

--- tests/models/test_course.py
+++ tests/models/test_course.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from models.course import Course
 
+
 class FakeTeacher:
     def __init__(self, name="Mme Dupont"):
         self.name = name
@@ -10,15 +11,17 @@ class FakeTeacher:
     def __str__(self):
         return self.name
 
+
 def test_str_output_without_teacher():
     course = Course(
-        name = "Physique",
-        start_date = date(2025, 9, 1),
-        end_date = date(2026, 6, 30)
+        name="Physique",
+        start_date=date(2025, 9, 1),
+        end_date=date(2026, 6, 30)
     )
     expected = "Physique (2025-09-01 – 2026-06-30),\npas d'enseignant affecté"
 
     assert str(course) == expected
+
 
 def test_str_output_with_teacher():
     course = Course(
