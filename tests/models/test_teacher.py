54042a8df56aecd34a588809d011ee72a0f3c49f
Rémi Campistron <remi.campistron@gmail.com>
2026-04-16 10:58:56 +0200

    Teste si un enseignant est ajouté à un cours

--- tests/models/test_teacher.py
+++ tests/models/test_teacher.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from models.course import Course
 from models.teacher import Teacher
 
 
@@ -11,3 +12,20 @@ def test_hiring_date_should_be_stored_properly():
         hiring_date=date(2025, 9, 1)
     )
     assert teacher.hiring_date == date(2025, 9, 1)
+
+
+def test_teacher_should_be_added_to_course():
+    teacher = Teacher(
+        first_name="Sophie",
+        last_name="Turner",
+        age=42,
+        hiring_date=date(2025, 9, 1)
+    )
+    course = Course(
+        name="Théatre",
+        start_date=date(2025, 9, 1),
+        end_date=date(2026, 6, 30)
+    )
+
+    teacher.add_course(course)
+    assert course.teacher == teacher
