eb8965e13065730c71fce174bf66610e38ddb5d2
Rémi Campistron <remi.campistron@gmail.com>
2026-04-16 10:19:57 +0200

    Vérifie si le cours est bien ajouté aux cours suivis par l'étudiant

--- tests/models/test_student.py
+++ tests/models/test_student.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from models.course import Course
 from models.student import Student
 
 
@@ -5,3 +8,14 @@ def test_student_number_should_increment():
     Student.students_nb = 0
     student = Student("Rémi","Campistron",43)
     assert student.student_nbr == 1
+
+
+def test_course_should_be_added_to_courses_taken():
+    student = Student("Rémi", "Campistron", 43)
+    course = Course(
+        name="Physique",
+        start_date=date(2025, 9, 1),
+        end_date=date(2026, 6, 30)
+    )
+    student.add_course(course)
+    assert len(student.courses_taken) == 1
