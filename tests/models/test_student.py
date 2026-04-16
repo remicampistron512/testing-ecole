c5938cb8618a41dfac82ca731b3676786df3eebe
Rémi Campistron <remi.campistron@gmail.com>
2026-04-16 10:23:01 +0200

    Vérifie si l'étudiant est bien ajouté à la liste des étudiants du cours

--- tests/models/test_student.py
+++ tests/models/test_student.py
@@ -19,3 +19,14 @@ def test_course_should_be_added_to_courses_taken():
     )
     student.add_course(course)
     assert len(student.courses_taken) == 1
+
+
+def test_student_should_be_added_to_student_taking_it():
+    student = Student("Rémi", "Campistron", 43)
+    course = Course(
+        name="Physique",
+        start_date=date(2025, 9, 1),
+        end_date=date(2026, 6, 30)
+    )
+    student.add_course(course)
+    assert student in course.students_taking_it
