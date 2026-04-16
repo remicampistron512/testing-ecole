1696dd4680a0703ebb4a08b5a179fd6b049e2b56
Rémi Campistron <remi.campistron@gmail.com>
2026-04-16 09:38:46 +0200

    Vérifie si la classe Person affiche  bien les données avec __str__

--- tests/models/test_person.py
+++ tests/models/test_person.py
@@ -1,3 +1,4 @@
+from models.address import Address
 from models.person import Person
 
 
@@ -7,4 +8,13 @@ def test_person_output_for_str_without_address():
     age = 43
     sut = Person(first_name,last_name,age)
     assert sut.__str__() == "Rémi Campistron (43 ans)"
+
+
+def test_person_output_for_str():
+    first_name = "Rémi"
+    last_name = "Campistron"
+    age = 43
+    sut = Person(first_name,last_name,age)
+    sut.address = Address("78,rue du Treuilh","TARTAS",40400)
+    assert sut.__str__() == "Rémi Campistron (43 ans), 78,rue du Treuilh, 40400 TARTAS"
 
